f04f6469c6572050fe4c0a186c021138530cf356
Gabriel Lima <gabriel58221@gmail.com>
2023-08-23 18:32:49 -0300

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
       <Header/>
 
-      <Post
-        author="Gabriel Lima"
-        content="lorem ipsum dolor sit amet, consectesstur adipaaa"
-      />
+      <div className={styles.wrapper}>
+        
+        <Sidebar/>
+
+        <main>
+        <Post
+          author="Gabriel Lima"
+          content="lorem ipsum dolor sit amet, consectesstur adipaaa"
+        />
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+            className={styles.cover} 
+            src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=200&q=40" 
+            alt="foto cover" 
+            />
+
+            <div className={styles.profile}>
+                <strong>Gabriel Lima</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
