4d04df2020e4b694f81d63b2ed0809c10102f7ef
Gabriel Lima <gabriel58221@gmail.com>
2023-08-25 17:50:25 -0300

    add props and date

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'http://github.com/diego3g.png',
+      name: 'Diego',
+      role: 'Software Student',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: '👉 jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-08-25 10:22:12'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'http://github.com/maykbrito.png',
+      name: 'Mayk',
+      role: 'Software Student',
+    },
+    content: [
+      { type: 'paragraph', content: 'Boa tarde pessoal 👋'},
+      { type: 'paragraph', content: 'Hoje é dia de codar!'},
+      { type: 'link', content: '👉 codding.backend/doctorcare'},
+    ],
+    publishedAt: new Date('2023-08-23 20:22:12'),
+  },
+]
+
 export function App() {
   return (
     <>
@@ -16,7 +47,17 @@ export function App() {
         <Sidebar/>
         
         <main>
-          <Post/>
+          {
+            posts.map(post => {
+              return (
+                <Post
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })
+          }
         </main>
 
       </div>
